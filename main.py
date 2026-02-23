1f66f0e8fbe20c8c7533bb6d03c966577a7bf971
SatyamWakhare15 <satyamwakhare1504@gmail.com>
2026-02-23 11:39:01 +0530

    updated features

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from starlette.exceptions import HTTPException as StarletteHTTPException
-
+from schemas import PostCreate,PostResponse
 
 app = FastAPI()
 
-app.mount("/static",StaticFiles(directory="static"),name="static")
+
+
+#app.mount("/static",StaticFiles(directory="static"),name="static")
 
 templates = Jinja2Templates(directory="templates")
 
@@ -36,6 +38,55 @@ def home(request: Request):
     return templates.TemplateResponse(request,"home.html",{"posts":posts,"title":"Home"}
     )
 
-@app.get("/api/posts")
+@app.get("/posts/{post_id}",include_in_schema=False)
+def post_page(request : Request,post_id : int):
+    for post in posts:
+        if post.get("id") == post_id:
+            title = post["title"][:50]
+            return templates.TemplateResponse(
+                request,
+                "post.html",
+                {"post":post,"title":title},
+
+            )
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Post not found")
+@app.get("/api/posts",response_model = list[PostResponse])
 def get_posts():
     return posts
+
+##
+## Create Post
+@app.get(
+    "/api/posts",
+    response_model = PostResponse,
+    status_code = status.HTTP_201_CREATED,
+
+)
+def create_post(post:PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "author":post.author,
+        "title" : post.title,
+        "content" : post.content,
+        "date_posted" : "April 23,2025",
+    }
+    posts.append(new_post)
+    return new_post
+##
+@app.get("/api/posts/{post_id}",response_model = PostResponse)
+def get_post(post_id:int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return post
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail="Post not found")
+    
+@app.exception_handler(StarletteHTTPException)
+def general_http_exception_handler(request:Request,exception:StarletteHTTPException):
+    message = (
+        exception.detail
+        if exception.detail
+        else "An error occured.Please check your request and try again."
+    )
+
+  #  @app.get => routes("/api/posts/{post_id}")
